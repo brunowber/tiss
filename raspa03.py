0e6cc1779d24f9b4ad1939fa669103c795c5adf7
Bruno Weber <bruno998>
2018-06-19 23:25:06 -0300

    Enviando email

--- raspa03.py
+++ raspa03.py
@@ -3,6 +3,8 @@
 
 from requests import get
 from bs4 import BeautifulSoup
+import smtplib
+from email.mime.text import MIMEText
 
 url = 'http://www.ans.gov.br/prestadores/tiss-troca-de-informacao-de-saude-suplementar/padrao-tiss-historico-das-versoes-dos-componentes-do-padrao-tiss'
 response = get(url)
@@ -18,5 +20,30 @@ if str(ultima_versao) == '<td>dez/17</td>':
 	print "TISS não atualizou"
 else:
 	print "TISS foi atualizado"
+	
+	
+# Open a plain text file for reading.  For this example, assume that
+# the text file contains only ASCII characters.
+fp = open('textfile.txt', 'rb')
+# Create a text/plain message
+msg = MIMEText(fp.read())
+fp.close()
+
+me = "Meu email"
+pswd = "minha senha"
+you = "para quem enviar"
+s
+# you == the recipient's email address
+msg['Subject'] = 'Assunto do email' 
+msg['From'] = me
+msg['To'] = you
+
+# Send the message via our own SMTP server, but don't include the
+# envelope header.
+server = smtplib.SMTP('smtp.gmail.com:587') #Servidor de email
+server.starttls() # inciar Serviço
+server.login(me, pswd) # Login
+server.sendmail(me, you, msg.as_string()) # Enviar, quem, para quem, mensagem
+server.close() # Fechar Conexão
 
 
