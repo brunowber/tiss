670beef0a887ea360462c3660014355696568e80
Bruno Weber <bruno998>
2018-06-19 23:26:05 -0300

    Correção email

--- raspa03.py
+++ raspa03.py
@@ -20,30 +20,23 @@ if str(ultima_versao) == '<td>dez/17</td>':
 	print "TISS não atualizou"
 else:
 	print "TISS foi atualizado"
-	
-	
-# Open a plain text file for reading.  For this example, assume that
-# the text file contains only ASCII characters.
-fp = open('textfile.txt', 'rb')
-# Create a text/plain message
-msg = MIMEText(fp.read())
-fp.close()
+	fp = open('textfile.txt', 'rb')
+	msg = MIMEText(fp.read())
+	fp.close()
 
-me = "Meu email"
-pswd = "minha senha"
-you = "para quem enviar"
-s
-# you == the recipient's email address
-msg['Subject'] = 'Assunto do email' 
-msg['From'] = me
-msg['To'] = you
+	me = "Meu email"
+	pswd = "minha senha"
+	you = "para quem enviar"
+	msg['Subject'] = 'Assunto do email' 
+	msg['From'] = me
+	msg['To'] = you
 
-# Send the message via our own SMTP server, but don't include the
-# envelope header.
-server = smtplib.SMTP('smtp.gmail.com:587') #Servidor de email
-server.starttls() # inciar Serviço
-server.login(me, pswd) # Login
-server.sendmail(me, you, msg.as_string()) # Enviar, quem, para quem, mensagem
-server.close() # Fechar Conexão
+	# Send the message via our own SMTP server, but don't include the
+	# envelope header.
+	server = smtplib.SMTP('smtp.gmail.com:587') #Servidor de email
+	server.starttls() # inciar Serviço
+	server.login(me, pswd) # Login
+	server.sendmail(me, you, msg.as_string()) # Enviar, quem, para quem, mensagem
+	server.close() # Fechar Conexão
 
 
